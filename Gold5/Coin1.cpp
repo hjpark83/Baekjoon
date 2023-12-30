d8a48b11d44d661641a77d8f77d5ef5b0a370265
Hyunjoon83 <junippini83@hanyang.ac.kr>
2023-12-30 13:42:42 +0900

    Hyunjoon

---
+++ Gold5/Coin1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+#define MAX 10001
+using namespace std;
+
+int N,K;
+int DP[MAX];
+vector<int> Coin;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin>>N>>K;
+    Coin.resize(N+1);
+
+    for(int i=1;i<=N;i++){
+        cin>>Coin[i];
+    }
+    DP[0]=1;
+
+    for(int i=1;i<=N;i++){
+        for(int j=Coin[i];j<=K;j++){
+            DP[j]+=DP[j-Coin[i]];
+        }
+    }
+    cout<<DP[K]<<'\n';
+    return 0;
+}
