e9026d21523691d9391b8580b8539ac28b44ef3d
Hyunjoon83 <junippini83@hanyang.ac.kr>
2024-01-13 11:40:20 +0900

    Hyunjoon

---
+++ Gold3/History.cpp
@@ -0,0 +1,50 @@
+/*문제 : https://www.acmicpc.net/problem/1613
+  알고리즘 : 그래프 이론, 최단 경로, 플로이드-워셜
+  티어 : Gold3
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N,K;
+    cin>>N>>K;
+    vector<vector<int>> v(N+1,vector<int>(N+1,0));
+
+    for(int i=0;i<K;i++){
+        int s,e;
+        cin>>s>>e;
+        v[s][e]=-1;
+        v[e][s]=1;
+    }
+
+    for(int k=1;k<=N;k++){
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                if(v[i][j]==0){
+                    if(v[i][k]==1 && v[k][j]==1){
+                        v[i][j]=1;
+                    }
+                    else if(v[i][k]==-1 && v[k][j]==-1){
+                        v[i][j]=-1;
+                    }
+                }
+            }
+        }
+    }
+
+    int S;
+    cin>>S;
+
+    for(int i=0;i<S;i++){
+        int a,b;
+        cin>>a>>b;
+        cout<<v[a][b]<<"\n";
+    }
+    return 0;
+}
