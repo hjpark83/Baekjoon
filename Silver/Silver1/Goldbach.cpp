2a0f5fcf0c1fd2d21f250c47e27c27c4030699d9
Hyunjoon83 <junippini83@hanyang.ac.kr>
2024-03-03 11:04:38 +0900

    Hyunjoon

--- Silver/Silver1/Goldbach.cpp
+++ Silver/Silver1/Goldbach.cpp
@@ -1,76 +1,52 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
+/*
+# Question: BJ 6588 (https://www.acmicpc.net/problem/6588)
+# Rank: Silver1
+# Algorithm: Eratosthenes, Prime Number, Math
+*/
 
-#define MAX_NUM 100000
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define MAX 1000000
 using namespace std;
 
-bool isOddPrime(int n) {
-    if (n<=1||n%2==0) {
-        return false;
-    }
-    for (int i=3;i*i<=n;i+=2) {
-        if (n%i==0){
-            return false;
-        }
-    }
-    return true;
-}
-int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int N;
+bool isPrime[MAX+1];
+vector<int> A(MAX+1, 0);
 
-    vector<int> A(MAX_NUM+1,1);
-    A[0]=A[1]=0;
-
-    for(int i=2;i<=sqrt(MAX_NUM);i++){
-        for(int j=i*i;j<=MAX_NUM;j+=i){
-            A[j]=0;
-        }
-    }
-    vector<int> B;
-    for(int i=3;i<=100;i++){
-        if(A[i]==1){
-            B.push_back(i);
-        }
-    }
+void Input() {
     
-    while(1){
-        int num;
-        cin>>num;
-        if(num==0){
-            break;
-        }
-        
-        bool found=false;
-        int i=0,j=B.size()-1;
-        int resultA=0, resultB=0;
+}
 
-        while(i<=j){
-            int sum=B[i]+B[j];
-            if(sum==num){
-                if(B[j]-B[i]>resultB-resultA && isOddPrime(B[j]) && isOddPrime(B[i])){
-                    resultA=B[i];
-                    resultB=B[j];
-                }
-                found=true;
-                i++;
-                j--;
-            }else if(sum>num){
-                j--;
-            }else if(sum<num){
-                i++;
-            }
+void Eratostenes(){
+    for(int i=2; i<=MAX; i++)
+        isPrime[i] = true;
+
+    for(int i=2; i<=sqrt(MAX); i++){
+        if(isPrime[i]){
+            for(int j=i*i; j<=MAX; j+=i)
+                isPrime[j] = false;
         }
-        if(found){
-            if(resultA!=0&&resultB!=0){
-                cout<<num<<" = "<<resultA<<" + "<<resultB<<'\n';
+    }
+}
+
+int main() {
+    fastio
+    Input();
+    
+    Eratostenes();
+    while(1){
+        cin>>N;
+        if(N == 0)
+            break;
+
+        for(int i=3; i<=N/2; i++){
+            if(isPrime[i] && isPrime[N-i]){
+                cout<<N<<" = "<<i<<" + "<<N-i<<"\n";
+                break;
             }else{
-                cout<<"Goldbach's conjecture is wrong.\n";
+                if(i == N/2)
+                    cout<<"Goldbach's conjecture is wrong.\n";
             }
-        }else{
-            cout<<"Goldbach's conjecture is wrong.\n";
         }
     }
     return 0;
